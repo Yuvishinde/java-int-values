47f7c3d8b41fae5facd47227a42b7194ecbd034d
Yuvraj <shindeyuvi123@gmail.com>
2021-09-22 23:32:13 +0530

    first commit

---
+++ Demo1.java
@@ -0,0 +1,20 @@
+
+
+
+class Demo1{
+
+    public static void main( String[] args){
+
+        int i=20;
+
+        if(i==10){
+               System.out.println("value match");
+
+        }
+        else{
+
+            System.out.println("value not match");
+        }
+    }
+}
+
